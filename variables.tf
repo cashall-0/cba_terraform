4091ab2e9cf6eee393c16940d50ad24c9fe6bfb9
siyaka promise <siyakapromise@gmail.com>
2024-12-21 18:13:45 +0100

    added featurees to deploy Ec2 with apache server

--- variables.tf
+++ variables.tf
@@ -1,21 +1,12 @@
 variable "region" {
-  default = "eu-west-1"
+  default = "eu-west-2"
 }
 
 variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "instance_ami" {
-  default = "ami-04f87c366aa353bc5"
-}
-
-
-variable "vpc_id" {
-  default = ""
-}
-
 
 variable "key_name" {
-  default = "cba_ireland_keypair"
+  default = "cba_keypair"
 }
